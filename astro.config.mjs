577a857d304c65263fd88c621c6c71cf4a3dec8c
L1lith <lily@l1lith.com>
2022-08-26 12:07:25 -0600

    make one of the icons inline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config'
-
 import sitemap from '@astrojs/sitemap'
-
+import mdx from '@astrojs/mdx'
 // https://astro.build/config
+
 export default defineConfig({
     site: 'https://melonjs.org',
-    integrations: [sitemap()],
+    integrations: [sitemap(), mdx()],
 })
