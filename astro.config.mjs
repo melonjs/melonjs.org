9d80a353004c804c1ab428a97eb60046c8690ebd
L1lith <lily@l1lith.com>
2022-09-27 20:54:27 -0600

    get the gallery carousel working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
-// https://astro.build/config
 
+import solidJs from '@astrojs/solid-js'
+
+// https://astro.build/config
 export default defineConfig({
     site: 'https://melonjs.org',
-    integrations: [sitemap(), mdx()],
+    integrations: [mdx(), solidJs(), sitemap()],
 })
