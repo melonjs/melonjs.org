96e01e9c49f9243d412e310db24aabb0d627a5a0
L1lith <lily@l1lith.com>
2022-10-14 16:03:59 -0600

    fix bug in script

--- translation/convert-to-json.js
+++ translation/convert-to-json.js
@@ -3,13 +3,17 @@ const { join } = require('path')
 const { parse } = require('node-html-parser')
 const { writeFileSync, readFileSync } = require('fs')
 const files = glob.sync('**/*.html', { cwd: __dirname })
-const translations = {}
 const langRegex = /(?=[^\.]+\.)[^\.]+(?=\.html$)/
 
 const queryMap = {
     introTitle: '.jumbotron .container h1',
     introPart1: '.jumbotron .container p:nth-of-type(1)',
     introPart2: '.jumbotron .container p:nth-of-type(2)',
+    benefitsSection: {
+        one: {
+            title: '.band:nth-of-type(3) .col-md-4 h2',
+        },
+    },
 }
 
 files.forEach((file) => {
@@ -17,8 +21,24 @@ files.forEach((file) => {
     const outputPath = join(__dirname, 'json', lang + '.json')
     const outputData = {}
     const doc = parse(readFileSync(join(__dirname, file)))
-    Object.entries(queryMap).forEach(([piece, query]) => {
-        outputData[piece] = doc.querySelector(query).innerText.trim()
-    })
+    const getPiece = (piece, query, target) => {
+        if (typeof query == 'object' && query !== null) {
+            if (!target.hasOwnProperty(piece)) target[piece] = {}
+            const newTarget = target[piece]
+            Object.entries(query).forEach(([piece, query]) => {
+                getPiece(piece, query, newTarget)
+            })
+        } else {
+            const element = doc.querySelector(query)
+            if (!element)
+                return console.warn(
+                    `Could not find the target using query ${query}`
+                )
+            target[piece] = element.innerText.trim()
+        }
+    }
+    Object.entries(queryMap).forEach(([piece, query]) =>
+        getPiece(piece, query, outputData)
+    )
     writeFileSync(outputPath, JSON.stringify(outputData))
 })
